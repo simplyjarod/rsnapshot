2403b61667c2b87bf3109bdcd8928e84a2747197
simplyjarod <simplyjarod@gmail.com>
2018-01-13 13:40:27 +0100

    Added crontab settings

--- setup.sh
+++ setup.sh
@@ -14,3 +14,9 @@ chmod 600 /etc/rsnapshot.conf
 
 # setup cron jobs
 echo "CRON JOBS SETUP REQUIRED"
+
+# run "crontab -e" and add these lines:
+# 0  */4  *  *  *  /usr/bin/rsnapshot hourly
+# 30  4   *  *  *  /usr/bin/rsnapshot daily
+# 0   5   *  *  7  /usr/bin/rsnapshot weekly
+# 30  5   1  *  *  /usr/bin/rsnapshot monthly
